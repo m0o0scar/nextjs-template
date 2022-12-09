396f5251ad16b1206cf570cf8bba96e824a5e37d
Oscar <tangqh@sea.com>
2022-12-09 18:02:49 +0800

    feat: setup three fiber xr template

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,43 @@
 import type { NextPage } from 'next';
-import { LoginButton } from '../components/commons/LoginButton';
+import { Vector3 } from 'three';
+import { Canvas } from '@react-three/fiber';
+import { VRButton, XR, Controllers, Hands } from '@react-three/xr';
+import { OrbitControls } from '@react-three/drei';
+
+const defaultStartPosition = new Vector3(0, 1.5, -1);
+
+const StageContent = () => {
+  return (
+    <>
+      <group position={defaultStartPosition}>
+        <mesh>
+          <boxGeometry args={[0.1, 0.1, 0.1]} />
+          <meshStandardMaterial color="orange" />
+        </mesh>
+      </group>
+    </>
+  );
+};
 
 const Home: NextPage = () => {
   return (
-    <article className="prose">
-      <h1>Hello World</h1>
-      <p>This is a NextJS demo</p>
-      <p>
-        <LoginButton />
-      </p>
-    </article>
+    <>
+      <VRButton />
+      <Canvas linear flat className="fixed left-0 right-0 top-0 bottom-0">
+        <color attach="background" args={[0.2, 0.2, 0.2]} />
+
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+
+        <OrbitControls maxDistance={0.3} target={defaultStartPosition} />
+
+        <XR>
+          <Controllers rayMaterial={{ opacity: 0 }} />
+          <Hands />
+          <StageContent />
+        </XR>
+      </Canvas>
+    </>
   );
 };
 
