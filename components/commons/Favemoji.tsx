3b1728a6492818e864763b477d85a3c8e4c41781
Oscar <tangqh@sea.com>
2023-05-29 14:22:34 +0800

    feat(component): add fav-emoji

---
+++ components/commons/Favemoji.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+
+export interface FavemojiProps {
+  emoji: string;
+}
+
+export const Favemoji: FC<FavemojiProps> = ({ emoji }) => {
+  return (
+    <link
+      rel="icon"
+      href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${emoji}</text></svg>`}
+    ></link>
+  );
+};
