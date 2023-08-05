faec35b08152c855ae8c5afeb0994e3ed0d689b1
Oscar <tangqh@sea.com>
2023-08-05 17:09:57 +0800

    feat: support WebAssembly

--- next.config.js
+++ next.config.js
@@ -5,13 +5,13 @@ const nextConfig = {
   swcMinify: true,
 
   webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
+    // support WebAssembly
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true,
+    };
     return config;
-  }
+  },
 };
 
 module.exports = nextConfig;
